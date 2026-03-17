8dfb305d82d9ef96720ab65c440de10bbcab8eb8
ffluryy <kaijie.chong@gmail.com>
2026-03-18 01:06:41 +0800

    Add CreateCommandTest
    Tests simple case

---
+++ src/test/java/dextro/command/CreateCommandTest.java
@@ -0,0 +1,32 @@
+package dextro.command;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import dextro.model.record.StudentDatabase;
+import dextro.model.Student;
+
+class CreateCommandTest {
+
+    @Test
+    void execute_validInput_studentAddedAndMessageCorrect() {
+        // Arrange
+        StudentDatabase db = new StudentDatabase();
+        CreateCommand cmd = new CreateCommand(
+                "John", "12345678", "john@mail.com", "house", "CS"
+        );
+
+        CommandResult result = cmd.execute(db);
+
+        assertEquals(1, db.getStudentCount());
+
+        Student student = db.getStudent(0);
+        assertEquals("John", student.getName());
+        assertEquals("12345678", student.getPhone());
+        assertEquals("john@mail.com", student.getEmail());
+        assertEquals("house", student.getAddress());
+        assertEquals("CS", student.getCourse());
+
+        assertEquals("Student created: John", result.getMessage());
+    }
+}
